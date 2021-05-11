933ede77265f8872705dfb8d74a0dc2247baf40f
celeste yost <my7998@rit.edu>
2021-05-10 20:34:54 -0400

    master: initial files

---
+++ mobileAppFinalProject/mobileAppFinalProject/mobileAppFinalProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  mobileAppFinalProjectApp.swift
+//  mobileAppFinalProject
+//
+//  Created by student on 5/10/21.
+//
+
+import SwiftUI
+
+@main
+struct mobileAppFinalProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
