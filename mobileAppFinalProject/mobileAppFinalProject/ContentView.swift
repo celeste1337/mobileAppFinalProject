ac428050ff33a69782c3e2f7ce64ee800af12b8d
celeste yost <my7998@rit.edu>
2021-05-12 13:49:21 -0400

    added icons & splash page

--- mobileAppFinalProject/mobileAppFinalProject/ContentView.swift
+++ mobileAppFinalProject/mobileAppFinalProject/ContentView.swift
@@ -25,44 +25,25 @@ struct ContentView: View {
     @State var selectedTab = Tabs.home
     
     var body: some View {
-            //navigate to the other page
-            //set state to returning
-        //if user.getHeight() == 0.0 {
-//            NavigationView {
-//                ZStack{
-//                    Image("Background")
-//                        .resizable()
-//                        .aspectRatio(contentMode: .fill)
-//                        .edgesIgnoringSafeArea(.all)
-//
-//                    VStack {
-//                        Text("here is some placeholder text")
-//
-//                        NavigationLink(
-//                            destination: QuestionPageView(),
-//                            label: {
-//                                Text("button lmao")
-//                            })
-//                    }
-//                }
-//            }
-        //} else if user.getHeight() != 0.0 {
                 TabView(selection: $selectedTab) {
                     HomePageView().tabItem {
+                        Image(systemName: "house")
                         Text("Home")
                     }.tag(Tabs.home)
                     HistoryPageView().tabItem {
+                        Image(systemName: "book")
                         Text("History")
                     }.tag(Tabs.history)
                     InfoPageView().tabItem {
+                        Image(systemName: "person")
                         Text("Info")
                     }.tag(Tabs.info)
                     
                 }.onAppear() {
-    UITabBar.appearance().barTintColor = UIColor(red: 67.0/255, green: 27.0/255, blue: 112.0/255, alpha: 1)
-    UITabBar.appearance().unselectedItemTintColor = UIColor.white
-    
-}
+                    UITabBar.appearance().barTintColor = UIColor(red: 67.0/255, green: 27.0/255, blue: 112.0/255, alpha: 1)
+                    UITabBar.appearance().unselectedItemTintColor = UIColor.white
+                    
+                }
 
 
 
---
+++ mobileAppFinalProject/mobileAppFinalProject/Views/SplashPageView.swift
@@ -0,0 +1,20 @@
+//
+//  SplashPageView.swift
+//  mobileAppFinalProject
+//
+//  Created by student on 5/12/21.
+//
+
+import SwiftUI
+
+struct SplashPageView: View {
+    var body: some View {
+        Text("welcome to our application")
+    }
+}
+
+struct SplashPageView_Previews: PreviewProvider {
+    static var previews: some View {
+        SplashPageView()
+    }
+}
